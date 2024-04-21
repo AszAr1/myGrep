d8ba560ecf183a8048815f785e6153367984b661
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-04-21 10:33:04 +0300

    getting regex string and file names

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,7 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -22,11 +20,24 @@ func init() {
 	grepCmd.Flags().BoolP("invert-match", "v", false, "Selects the non-matching lines of the provided input pattern")
 	grepCmd.Flags().BoolP("line-number", "n", false, "Prefix each line of the matching output with the line number in the input file")
 	grepCmd.Flags().BoolP("word-regexp", "w", false, "Find the exact matching word from the input file or string")
-	grepCmd.Flags().BoolP("count", "c", false, "Count the number of occurrences of the provided pattern instead of outputing the lines")
+	grepCmd.Flags().BoolP("count", "c", false, "Count the number of occurrences of the provided pattern instead of outputting the lines")
 }
 
 func grep(cmd *cobra.Command, args []string) {
+	regex, fileNames := getRegexAndFileNames(args)
+}
 
+func check(err error, errString string) {
+	if err != nil {
+		log.Fatal(errString, err)
+	}
+}
+
+func getRegexAndFileNames(args []string) (string, []string) {
+	if len(args) == 0 {
+		log.Fatal("Have not been given any file names")
+	}
+	return args[0], args[1:]
 }
 
 func Execute() {
